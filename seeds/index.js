2b029ac825b298e2dce23777d7ca62a4e6bf0fe1
Boon0710 <minhhieutcd@gmail.com>
2024-06-02 23:16:32 +0700

    styles

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus aperiam ad voluptates quos, quam cupiditate perspiciatis facilis, expedita odio consectetur, at quas placeat. Quidem at, perspiciatis ipsum ab quod perferendis.',
+            price: price
         })
         await camp.save();
     }
