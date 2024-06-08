79ab4a2e94cceb8074990f434a37c8f80f0e7492
Boon0710 <minhhieutcd@gmail.com>
2024-06-08 23:29:25 +0700

    Review Function

--- schemas.js
+++ schemas.js
@@ -1,12 +1,19 @@
 //this is not database schema, this is schema for validation on server side
 const Joi = require('joi')
 
-module.exports.campgroundSchema = campgroundSchema = Joi.object({
+module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
+    }).required()
+})
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        body: Joi.string().required(),
+        rating: Joi.number().required()
     }).required()
 })
